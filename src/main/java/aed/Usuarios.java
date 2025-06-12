ec67c0ae13aeb9472fe8101c6cc9c634ccd94666
leanflores <85359900+leanflores@users.noreply.github.com>
2025-06-11 22:21:47 -0300

    Update Usuarios.java

--- src/main/java/aed/Usuarios.java
+++ src/main/java/aed/Usuarios.java
@@ -1,44 +1,64 @@
 package aed;
 
-public class Usuarios implements Comparable<Usuarios>{ //arreglo ordenado --> se guarda en un heap
+public class Usuarios{
     private Heap<Usuario> heapSaldos;
-    private Usuario[] arregloUsuarios;  
-    // inicializador/constructor
+    private Usuario[] arregloUsuarios;
+
+    public Usuarios(int cant) {
+            this.arregloUsuarios = new Usuario[cant + 1];
+    }
+
+    public void agregarUsuario(int id, int saldoInicial) {
+        Usuario u = new Usuario(id, saldo);
+        arregloUsuarios[id] = u;
+    }
+
+    public Usuario obtenerUsuarioConMasSaldo() {
+        throw new UnsupportedOperationException("Implementar!");
+    }
+
+    public Usuario obtenerUsuarioPorId(int id) {
+        return arregloUsuarios[id];
+    }
+
+    public void modificarSaldo(int id, int nuevoSaldo) {
+        throw new UnsupportedOperationException("Implementar!");
+    }
+}
+
+public class Usuario implements Comparable<Usuario>{
     int id;
     int saldo;
 
-    public Usuarios (int id) {
+    public Usuario (int id, int saldoInicial){
         this.id = id;
-        this.saldo = 0;
+        this.saldo = saldoInicial;
     }
-    
 
-    public int getId() { return id; }
-    public int getSaldo() { return saldo; }
+    public int obtenerId(){
+        return id;
+    }
 
+    public int obtenerSaldo(){
+        return saldo;
+    }
 
     @Override
-    public int compareTo(Usuarios otro) {
-    throw new UnsupportedOperationException("Implementar!");
+    public int compareTo(Usuario otro){
+        if(this.saldo > otro.saldo){
+            return 1;
+        }
+        else if(this.saldo < otro.saldo){
+            return -1;
+        }
+        else{
+            return 0;
+        }
     }
 
+    @Override
+    public boolean equals(Object otro){
+        throw new UnsupportedOperationException("Implementar!");
     }
-       
-        /*
-         
-        
-        }
-        
-        @Override
-        public boolean equals(Object otro){
-            throw new UnsupportedOperationException("Implementar!");
-        }
-        
-        */
-
-public class Usuario implements Comparable<Usuarios>{
-    int id;
-    int saldo;
-
-    public Void
+}
 
